75443993e7fd66c7bf941e60374f2535111a0d0c
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-03-22 13:37:03 -0700

    Intro To JUnit IP

---
+++ src/IntroToJUnit.java
@@ -0,0 +1,22 @@
+
+public class IntroToJUnit 
+{	
+	
+}
+/*
+Get a red marker, a green marker, and another color to write text.
+Write at top of board:
+               assertEquals(expected, actual)
+
+If expected matches actual, the results bar will be green, if they are different, the results bar will be red.
+Ask the students, will the results bar be green or red for the following asserts?
+        assertEquals(true, water is wet)
+        assertEquals(true, zombies are real)
+        assertEquals(false, coding is lame)
+Part 2
+Now for some real code. Will the results bar be green or red for these asserts?
+       assertEquals(4, add(2,2));
+       assertEquals(“brains”, getZombieFood());
+       assertEquals(2, getLargest(3,2));
+Copyright League of Amazing Programmers 2013-2017 
+*/
